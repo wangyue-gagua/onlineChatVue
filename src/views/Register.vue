2ba425ab22367a0ee1a25e456be563d744dd6c0d
wangyue-gagua <2503308794@qq.com>
2022-11-28 22:00:45 +0800

    create register view page

---
+++ src/views/Register.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { auth } from '@/firebaseConfig';
+import { createUserWithEmailAndPassword } from '@firebase/auth';
+import { ref } from 'vue';
+
+const email = ref('');
+const password = ref('');
+const snackbar = ref(false);
+
+const chatAuth = auth
+const emailCreateCount = () => {
+    createUserWithEmailAndPassword(chatAuth, email.value, password.value)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            // ...
+            console.log(user)
+            snackbar.value = true
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+            console.log(errorCode, errorMessage)
+        });
+    // console.log(email.value, password.value)
+}
+</script>
+
+<template>
+    <v-form>
+        <v-container>
+            <v-text-field v-model="email" label="Email" required>
+            </v-text-field>
+            <v-text-field v-model="password" label="Password" required>
+            </v-text-field>
+            <v-btn @click="emailCreateCount">
+                Register
+            </v-btn>
+        </v-container>
+    </v-form>
+    <v-snackbar v-model="snackbar" :timeout=500>
+        Sign in おめでとう
+    </v-snackbar>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
