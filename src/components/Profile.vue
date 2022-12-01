9c696ef18deef3852fc5646ebb9d01d38d079afc
wang yue <72451727+wangyue-gagua@users.noreply.github.com>
2022-12-01 16:10:05 +0000

    impl user info component

---
+++ src/components/Profile.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-card>
+        <v-list-item prepend-avatar="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg" :title="title"
+            :subtitle="subtitle"></v-list-item>
+    </v-card>
+</template>
+<script setup lang="ts">
+defineProps<{
+    title: string,
+    subtitle: string,
+}>()
+</script>
+
+<style lang="scss" scoped>
+
+</style>
